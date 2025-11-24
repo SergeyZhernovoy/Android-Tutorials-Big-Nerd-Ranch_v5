de7a1cfddc04b04c3026d504d1f30281a5914758
Sergey Zhernovoy <zhernovoy_sa@nlmk.com>
2025-11-24 08:51:39 +0300

    refactoring and polish

--- CodaPizza/app/src/main/java/com/learning/codapizza/MainActivity.kt
+++ CodaPizza/app/src/main/java/com/learning/codapizza/MainActivity.kt
@@ -6,24 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.learning.codapizza.model.Pizza
 import com.learning.codapizza.ui.PizzaBuilderScreen
-import com.learning.codapizza.ui.PizzaSizeDropDownDialog
 import com.learning.codapizza.ui.theme.CodaPizzaTheme
 
 class MainActivity : ComponentActivity() {
--- CodaPizza/app/src/main/java/com/learning/codapizza/ui/PizzaBuilderScreen.kt
+++ CodaPizza/app/src/main/java/com/learning/codapizza/ui/PizzaBuilderScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.toUpperCase
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.learning.codapizza.R
 import com.learning.codapizza.model.Pizza
